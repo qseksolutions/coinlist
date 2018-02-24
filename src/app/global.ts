a2193422487883df24c760455207a2fd0f5dfd44
Jaydip Patel <jaydip@qseksolutions.com>
2018-02-24 19:05:05 +0530

    no message

--- src/app/global.ts
+++ src/app/global.ts
@@ -39,11 +39,13 @@ export const login_ses = localStorage.getItem('login_ses');
 export const userid = localStorage.getItem('id');
 export const useremail = localStorage.getItem('email');
 export const username = localStorage.getItem('name');
+export const role = localStorage.getItem('role');
 export const usertype = localStorage.getItem('usertype');
 export const userstatus = localStorage.getItem('status');
 export const basecurr = localStorage.getItem('base');
 export const base_sing = localStorage.getItem('base_sing');
 export const user_base = localStorage.getItem('user_base');
+export const token = localStorage.getItem('token');
 const aurl = window.location.href;
 const burl = aurl.split('/');
 if (burl['3'] !== 'coin' || burl['3'] !== 'followlist' || burl['3'] !== 'portfolio' || burl['3'] !== 'profile') {
